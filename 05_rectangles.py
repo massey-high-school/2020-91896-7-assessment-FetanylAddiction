1c4442b4dfd17822ebce903fda19aa7db86f0257
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-06-25 09:54:34 +1200

    05_rectangles
    Shape caculator for Rectangles

---
+++ 05_rectangles.py
@@ -0,0 +1,31 @@
+# **** Functions ****
+
+# shape ask function:
+
+
+# square height and base function
+
+def rectangle_formula():
+    width = float(input("Width length?: "))
+    height = float(input("Height length?: "))
+    var_area = width * height
+    var_perimeter = width + height * 2
+
+    return [var_area, var_perimeter]
+
+# *** Main Routine starts here ***
+
+valid_shapes = ["square", "rectangle", "triangle", "circle"]
+
+shape = "rectangle"
+
+print("You chose:", shape)
+
+if shape == "rectangle":
+    rectangle_ans = rectangle_formula()
+    area = rectangle_ans[0]
+    perimeter = rectangle_ans[1]
+    print(rectangle_ans, perimeter, area)
+
+elif shape == "rectangle":
+    print("you have selected rectangle")
