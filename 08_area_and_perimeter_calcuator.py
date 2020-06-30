bc45f47e7aff0978ca183c5cfe33cb2cf8330f7e
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-06-30 12:15:35 +1200

    Update 08_area_and_perimeter_calcuator.py

--- 08_area_and_perimeter_calcuator.py
+++ 08_area_and_perimeter_calcuator.py
@@ -1,4 +1,24 @@
-# shape input
+# Num_check
+
+
+def num_check(question):
+    error = "Please enter a number that is more than zero"
+
+    valid = False
+    while not valid:
+
+        try:
+            response = float(input(question))
+            if response <= 0:
+                print(error)
+            else:
+
+                return response
+
+        except ValueError:
+            print(error)
+
+# shape input/ string checker
 
 
 def string_checker(question, to_check):
@@ -16,10 +36,9 @@ def string_checker(question, to_check):
         print("sorry that is not a valid response")
 
 
-# *** Main Routine starts here ***
-
 yes_no = ["square", "rectangle", "triangle", "circle"]
-
+print("[Square][Rectangle][Triangle][Circle]")
+print()
 shape = string_checker("Choose a shape ", yes_no)
 
 print("You chose:", shape)
@@ -27,9 +46,9 @@ print("You chose:", shape)
 
 # square input
 
-
+# Ask user for the squares side length and calculates it
 def square_formula():
-    side = float(input("Side length?"))
+    side = num_check("Side length?")
     var_area = side * side
     var_perimeter = side * 4
 
@@ -38,10 +57,10 @@ def square_formula():
 
 # rectangle input
 
-
+# Ask user for the rectangle's length and height and calculates it
 def rectangle_formula():
-    width = float(input("Width length?: "))
-    height = float(input("Height length?: "))
+    width = num_check("Width length?: ")
+    height = num_check("Height length?: ")
     var_area = width * height
     var_perimeter = (width + height) * 2
 
@@ -50,43 +69,66 @@ def rectangle_formula():
 
 # triangle input
 
-
+# Ask user for the base and height and calculates it
 def triangle_formula():
-    height = float(input("Height length?: "))
-    length = float(input("Base Length?"))
-    var_area = length / 2 * height
-    var_perimeter = height * 2 + length
+    height = num_check("Height length?: ")
+    base = num_check("Base Length?")
+    var_area = base / 2 * height
+    var_perimeter = height * 2 + base
 
     return [var_area, var_perimeter]
 
 
 # circle input
-
-
 pi = 3.14
 
 
+# Ask user for the radius of the circle and calculates it
 def circle_formula():
-    radius = float(input("What is the Radius?: "))
+    radius = num_check("What is the Radius?: ")
     var_area = pi * radius * radius
     var_perimeter = 2 * pi * radius
 
     return [var_area, var_perimeter]
 
 
-# Main Routine
+# ***** Main Routine *****
 
 
 valid_shapes = ["square", "rectangle", "triangle", "circle"]
 
-# shape response
-print("You chose:", shape)
+# square response
 
+# Prints square calculator results into a list with a max of two decimal points
 if shape == "square":
     square_ans = square_formula()
     area = square_ans[0]
     perimeter = square_ans[1]
-    print(square_ans)
+    print("Area: {:.2f} | Perimeter: {:.2f}".format(area, perimeter))
 
-elif shape == "rectangle":
-    print()
+# rectangle response
+
+# Prints rectangle calculator results into a list with a max of two decimal points
+if shape == "rectangle":
+    rectangle_ans = rectangle_formula()
+    area = rectangle_ans[0]
+    perimeter = rectangle_ans[1]
+    print("Area: {:.2f} | Perimeter: {:.2f}".format(area, perimeter))
+
+# triangle response
+
+# Prints triangle calculator results into a list with a max of two decimal points
+if shape == "triangle":
+    triangle_ans = triangle_formula()
+    area = triangle_ans[0]
+    perimeter = triangle_ans[1]
+    print("Area: {:.2f} | Perimeter: {:.2f}".format(area, perimeter))
+
+# circle response
+
+# Prints circle calculator results into a list with a max of two decimal points
+if shape == "circle":
+    circle_ans = circle_formula()
+    area = circle_ans[0]
+    perimeter = circle_ans[1]
+    print("Area: {:.2f} | Perimeter: {:.2f}".format(area, perimeter))
